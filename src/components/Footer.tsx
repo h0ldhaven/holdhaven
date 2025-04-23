2babfa85299cafefbb7eb039b847dca024bb7748
h0ldhaven <h0ldhaven@users.noreply.github.com>
2025-04-23 15:46:10 +0200

    add some components

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import { JSX } from 'react';
+
+export default function Footer(): JSX.Element {
+    return(
+        <footer className='w-full px-4 py-6 mt-auto text-center text-white bg-cyan-800'>
+
+        </footer>
+    );
+};
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 const HomePage: React.FC = () => {
     return (
-        <main className='flex flex-col h-full min-h-screen bg-gray-800' role='main'>
+        <main className='flex flex-col h-full min-h-screen bg-blue-800' role='main'>
+            <Header />
             <div className='flex flex-col justify-center items-center text-center mt-8 text-white font-roboto'>
                 <h1 className='font-bold text-4xl p-4'>Bienvenue sur holdhaven.fr !</h1>
                 <h3 className='text-2xl p-2'>Le site est actuellement en construction, merci de repasser plus tard</h3>
             </div>
+            <Footer />
         </main>
     );
 };
 
-export default HomePage;
+export default HomePage; 
