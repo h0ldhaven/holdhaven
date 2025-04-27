553ff845bb46490d49463680a694c9081352bd7c
h0ldhaven <h0ldhaven@users.noreply.github.com>
2025-04-27 16:34:17 +0200

    ✨ New: Project and Stream page

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import ErrorPage from './pages/ErrorPage';
 import HomePage from './pages/HomePage';
+import StreamPage from './pages/StreamPage';
+import ProjectPage from './pages/ProjectPage';
 
 const RouterApp: React.FC = () => {
     return (
@@ -10,6 +12,8 @@ const RouterApp: React.FC = () => {
             <Routes>
                 <Route path='*' element={<ErrorPage />} />
                 <Route path='/' element={<HomePage />} />
+                <Route path='/streams' element={<StreamPage />} />
+                <Route path='/projects' element={<ProjectPage />} />
             </Routes>
         </Router>
     );
---
+++ src/pages/StreamPage.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+
+const StreamPage: React.FC = () => {
+    return(
+        <main className='flex flex-col h-full min-h-screen bg-blue-800'>
+            <Header />
+            <div className='flex flex-col justify-center items-center text-center mt-8 text-white font-roboto'>
+                <h1 className='font-bold text-4xl p-4'>Bienvenue sur holdhaven.fr/streams !</h1>
+            </div>
+            <Footer />
+        </main>
+    );
+};
+
+export default StreamPage;
