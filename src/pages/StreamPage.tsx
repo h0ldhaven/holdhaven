f25d5bdcaa60937e3fb5e15df558b81af2e7a09f
h0ldhaven <h0ldhaven@users.noreply.github.com>
2025-05-06 10:06:37 +0200

    💄 Update background and text color for light and dark mode

--- src/pages/StreamPage.tsx
+++ src/pages/StreamPage.tsx
@@ -5,9 +5,9 @@ import Footer from '../components/Footer';
 
 const StreamPage: React.FC = () => {
     return(
-        <main className='flex flex-col h-full min-h-screen bg-blue-800'>
+        <main className='flex flex-col h-full min-h-screen bg-white text-black dark:bg-gray-800 dark:text-white'>
             <Header />
-            <div className='flex flex-col justify-center items-center text-center mt-8 text-white font-roboto'>
+            <div className='flex flex-col justify-center items-center text-center mt-8 font-roboto'>
                 <h1 className='font-bold text-4xl p-4'>Bienvenue sur holdhaven.fr/streams !</h1>
             </div>
             <Footer />
