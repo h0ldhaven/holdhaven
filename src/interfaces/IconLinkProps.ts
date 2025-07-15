2fa294a6b299e0f9f4a7490241ad025c4702dd13
h0ldhaven <h0ldhaven@users.noreply.github.com>
2025-07-15 18:47:25 +0200

    ♻️ Refactoring of IconLink Component

---
+++ src/interfaces/IconLinkProps.ts
@@ -0,0 +1,9 @@
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+
+export interface IconLinkProps {
+    link: string;
+    icon: IconDefinition;
+    className?: string;
+    name?: string;
+    label?: string;
+}
